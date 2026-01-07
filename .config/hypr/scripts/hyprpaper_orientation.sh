0d72bd99aa0ee2d4956308dc91e9dffe898ee9af
pbrentlinger <pbrentlinger@gmail.com>
2026-01-07 12:38:39 -0500

    moved wallpaper rotation script to use IPC for live changes.

--- .config/hypr/scripts/hyprpaper_orientation.sh
+++ .config/hypr/scripts/hyprpaper_orientation.sh
@@ -1,41 +1,50 @@
 #!/bin/bash
 
 CONFIG_PATH="$HOME/.config/hypr/hyprpaper.conf"
+NAME=""
+WALLPAPER=""
 
-# Generate config
-> "$CONFIG_PATH"
+echo "Initial = $NAME, $WALLPAPER" 
+
+
+# Generate wallpapers dynamically via hyprpaper IPC
 hyprctl monitors -j | jq -c '.[]' | while read -r MON; do
+
+    echo "wallpaper = $NAME, $WALLPAPER" 
+
     NAME=$(echo "$MON" | jq -r '.name')
+
+    echo "Name Init = $NAME, $WALLPAPER" 
+
     ORIENTATION=$(echo "$MON" | jq -r '.transform')
-    WALLPAPER=""
     case "$NAME" in
         "DP-1")
             if [[ "$ORIENTATION" == "0" || "$ORIENTATION" == "2" ]]; then
                 WALLPAPER="$HOME/.config/hypr/static/mind_of_christ_4k.jpg"
+                echo "Wallpaper Landscape = $NAME, $WALLPAPER" 
             else
                 WALLPAPER="$HOME/.config/hypr/static/mind_of_christ_max_vert.jpg"
+                echo "Wallpaper portrait = $NAME, $WALLPAPER" 
             fi
             ;;
         "HDMI-A-1")
             if [[ "$ORIENTATION" == "0" || "$ORIENTATION" == "2" ]]; then
                 WALLPAPER="$HOME/.config/hypr/static/1080p/mind_of_christ_1080p.jpg"
+                echo "Wallpaper Landscape = $NAME, $WALLPAPER" 
             else
                 WALLPAPER="$HOME/.config/hypr/static/1080p/mind_of_christ_vert_1080p.jpg"
+                echo "Wallpaper portrait = $NAME, $WALLPAPER" 
             fi
             ;;
         *)
             WALLPAPER="$HOME/.config/hypr/static/mind_of_christ_max.jpg"
             ;;
     esac
-    echo "preload = $WALLPAPER" >> "$CONFIG_PATH"
-    echo "wallpaper = $NAME, $WALLPAPER" >> "$CONFIG_PATH"
+    # echo "preload = $WALLPAPER" >> "$CONFIG_PATH"
+
+    # Set wallpaper via hyprpaper IPC
+    hyprctl hyprpaper wallpaper "${NAME}, ${WALLPAPER}, cover"
 done
 
-# Kill all running hyprpaper processes
-pkill -x hyprpaper
+echo "wallpaper = $NAME, $WALLPAPER" 
 
-# Wait a moment to ensure process is dead
-sleep 0.5
-
-# Restart hyprpaper with the new config
-hyprpaper -c "$CONFIG_PATH" &
