c9ca8ad6e3a74a517f5f59c9f870984871490d5c
pbrentlinger <pbrentlinger@gmail.com>
2026-02-13 18:15:38 -0500

    touchups on configs

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -156,6 +156,8 @@ alias :q 'exit'
 alias quit 'exit'
 alias cls 'clear'
 alias cmatrix 'cmatrix -b -u 7'
+alias whattime 'date +%I:%M%P'
+alias v 'nvim'
 
 # Get fastest mirrors
 alias mirror 'sudo reflector -f 30 -l 30 --number 10 --verbose --save /etc/pacman.d/mirrorlist'
